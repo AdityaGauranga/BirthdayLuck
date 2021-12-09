d5ef69a5e38353757815139d108f62435fb450ac
AdityaGauranga <aaditya.sarma108@gmail.com>
2021-12-09 17:30:34 +0530

    comparing sum n lucky no

--- script.js
+++ script.js
@@ -2,13 +2,22 @@ const dateOfBirth = document.querySelector("#date-of-birth");
 const luckyNo = document.querySelector("#lucky-number");
 const checkNoButton = document.querySelector("#check-number");
 
+function compareValues(sum, luckyNo){
+    if (sum%luckyNo === 0){
+        console.log("Your birthday is lucky （￣︶￣)")
+    }
+    else {
+        console.log("Sorry an unlucky birthday it seems ＞﹏＜");
+    }
+}
+
 function checkBDayIsLucky() {
     const dob = dateOfBirth.value;
     const sum = calculateSum(dob);
-    console.log(sum);
+    compareValues(sum, luckyNo.value)
 }
 
-function calculateSum(date) {
+function calculateSum(dob) {
     dob = dob.replaceAll("-","");
     let sum = 0;
     for (let index = 0; index < dob.length; index++) {
